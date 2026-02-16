b106961a48a88b8ce5e4664c8c30dbc011c20b42
PrevinBogopa <previnbogopa@icloud.com>
2026-02-16 21:01:59 +0200

    Day4 host s3 state file

---
+++ D4-AWS/main.tf
@@ -0,0 +1,19 @@
+terraform {
+    backend "s3" {
+        bucket = "cloud-with-previn-state-file"
+        key = "dev/terraform.tfstate"
+        region = "us-east-1"
+        encrypt = true
+        use_lockfile = true
+    }
+    required_providers{
+        aws ={
+            source="hashicorp/aws"
+            version= "~> 6.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
