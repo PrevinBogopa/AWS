8dae0f4d02c241d61de97bf7ef97fc5bf98ba4d9
PrevinBogopa <previnbogopa@icloud.com>
2026-02-16 20:43:42 +0200

    Day 3 all

--- D1-AWS/main.tf
+++ D1-AWS/main.tf
@@ -10,22 +10,5 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-# Create VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# Create Subnet
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-}
-
-# Create EC2 Instance
-resource "aws_instance" "example" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main.id
-}
+ 
+ 
